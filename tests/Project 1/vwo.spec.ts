9bed381e74009c3f5a9c0ce00a06f5c674d015b2
Palkitr <palkitrathod82@gmail.com>
2026-07-13 17:16:14 +0530

    project 1

--- tests/Project 1/vwo.spec.ts
+++ tests/Project 1/vwo.spec.ts
@@ -2,17 +2,26 @@
 
 import {test,expect} from 'playwright/test';
 {
+    //Traditional Locators like Xpath, CSS Selectors, ID, Class Name, Name, Tag Name, Link Text, Partial Link Text are used to locate the elements on the web page.
     test('Verify the error message is displayed when submitting an empty form', async ({ page }) => {
         await page.goto('https://vwo.com/free-trial/');
 
-        let inputBox =  page.locator("#page-v1-step1-email")
-        await inputBox.fill('jdhfjd@df.com');
+        let inputBox =  page.locator("#page-v1-step1-email") //CSS Selector
+        await inputBox.fill('jdhdf.com');
 
-        let checkBox = await page.locator('input[type="checkbox"]');
+        let checkBox = await page.locator('//input[@id="page-free-trial-step1-cu-gdpr-consent-checkbox"]');
         await checkBox.check();
 
-        let submitButton = await page.locator('button[type="submit"]');
-        await submitButton.click();
+        await page.locator("//button[@data-qa='page-su-submit']").first().click();
+
+
+        let error_msg = await page.locator("//div[contains(@class,'invalid-reason')]").first().textContent();
 
     });
+
+    // test('Verify the error message is displayed whren submitting an empty form', async ({ page }) => 
+    // {
+    //     await page.goto('https://vwo.com/free-trial/');
+    // })
 }
+
